05ebad3556810d24ac76f063d08e4df6b96cd03d
Pratiksha Pai <1312pratiksha@gmail.com>
2025-03-30 17:38:29 -0700

    Add MailboxList h1

--- src/components/MailboxList/MailboxList.jsx
+++ src/components/MailboxList/MailboxList.jsx
@@ -2,14 +2,19 @@ import { Link } from "react-router-dom";
 
 export default function MailboxList({ mailboxes }) {
   return (
-    <nav>
-      <ul>
-        {mailboxes.map((mailbox) => (
-          <li key={mailbox._id}>
-            <Link to={`/mailboxes/${mailbox._id}`}>Mailbox {mailbox._id}</Link>
-          </li>
-        ))}
-      </ul>
-    </nav>
+    <>
+      <h1>Mailbox List</h1>
+      <nav>
+        <ul>
+          {mailboxes.map((mailbox) => (
+            <li key={mailbox._id}>
+              <Link to={`/mailboxes/${mailbox._id}`}>
+                Mailbox {mailbox._id}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </>
   );
 }
