b35a80b38c7600928456ccdeba1ecb6219897b74
Pratiksha Pai <1312pratiksha@gmail.com>
2025-03-30 17:57:40 -0700

    Add MailboxForm without submit functionality

--- src/components/MailboxForm/MailboxForm.jsx
+++ src/components/MailboxForm/MailboxForm.jsx
@@ -1,7 +1,47 @@
+import { useState } from "react";
+
 export default function MailboxForm({ mailboxes }) {
+  const initialState = { boxOwner: "", boxSize: "Small" };
+  const [formData, setFormData] = useState(initialState);
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    // TODO : complete submit logic
+    console.log(formData);
+  };
+
+  const handleChange = ({ target }) => {
+    setFormData({ ...formData, [target.name]: target.value });
+  };
+
   return (
     <>
       <h1>New Mailbox</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="boxOwner">Enter a Boxholder:</label>
+        <input
+          type="text"
+          id="boxOwner"
+          name="boxOwner"
+          value={formData.boxOwner}
+          placeholder="Boxholder name"
+          onChange={handleChange}
+          required
+        />
+        <label htmlFor="weight">Weight:</label>
+        <select
+          type="number"
+          id="weight"
+          name="weight"
+          value={formData.weight}
+          onChange={handleChange}
+        >
+          <option value="Small">Small</option>
+          <option value="Medium">Medium</option>
+          <option value="Large">Large</option>
+        </select>
+        <button type="submit">Submit</button>
+      </form>
     </>
   );
 }
